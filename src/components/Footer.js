a1be24cf292efb224f5daadf31d10d1ec1cf1a0b
adraarda23 <ardaaydinkilinc@gmail.com>
2023-09-08 12:03:47 +0300

    responsive changes and improvements added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,6 +2,7 @@ import React from 'react'
 import "./Styles/Footer.css"
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import TwitterIcon from '@mui/icons-material/Twitter';
+import GitHubIcon from '@mui/icons-material/GitHub';
 
 
 
@@ -12,6 +13,7 @@ function Footer() {
   <div className='icons'>
     <a href="https://www.linkedin.com/in/ardaaydınkılınç/"><LinkedInIcon className='icon'/></a>
     <a href="https://twitter.com/ardaydinkilinc"><TwitterIcon className='icon'/></a>
+    <a href="https://github.com/adraarda23"><GitHubIcon/></a>
   </div>
   <div className='cr'><h4>Copyright2023© Arda Aydın Kılınç</h4></div>
 </div>
