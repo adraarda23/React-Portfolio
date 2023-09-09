9f11d1cf51f6fad36ecf58b72f3b4192c9d26e7c
adraarda23 <ardaaydinkilinc@gmail.com>
2023-09-09 08:57:59 +0300

    Navbar button animations added

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,41 @@
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import { NavLink } from "react-router-dom";
+import { NavLink,useLocation } from "react-router-dom";
 import "./Styles/NavBar.css";
 import MainLogo from "../images/main.png";
+import { useState,useEffect } from "react";
+
+
+function useActiveLink() {
+  const location = useLocation();
+  const [activeLink, setActiveLink] = useState(null);
+
+  useEffect(() => {
+    const activeNavLink = document.querySelector(".navLink.active");
+    if (activeNavLink) {
+      setActiveLink(activeNavLink);
+    } else {
+      setActiveLink(null);
+    }
+  }, [location.pathname]);
+
+  return activeLink;
+}
 
 function NavBar() {
+  const activeLink = useActiveLink();
+  console.log(activeLink)
+
+  const handleMouseEnter = () => {
+    activeLink.classList.remove("active");
+  };
+
+ 
+  const handleMouseLeave = () => {
+    activeLink.classList.add("active");
+  };
+  
   return (
     <nav className="mainNavbar">
       <Navbar expand="lg" className="bg-dark text-light">
@@ -16,18 +46,42 @@ function NavBar() {
           <Navbar.Toggle aria-controls="basic-navbar-nav" className="custom-toggler-icon" style={{ filter: 'brightness(0) invert(1)' }}/>
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto NavBar">
-              <NavLink className="navLink" to="/">
-                Home
-              </NavLink>
-              <NavLink className="navLink" to="/about">
-                About
-              </NavLink>
-              <NavLink className="navLink" to="/projects">
-                Projects
-              </NavLink>
-              <NavLink className="navLink" to="/contact">
-                Contact
-              </NavLink>
+            <NavLink
+        className="navLink navlink-home"
+        to="/"
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+        
+      >
+        Home
+      </NavLink>
+      <NavLink
+        className="navLink navlink-about"
+        to="/about"
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+        
+      >
+        About
+      </NavLink>
+      <NavLink
+        className="navLink navlink-project"
+        to="/projects"
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+        
+      >
+        Projects
+      </NavLink>
+      <NavLink
+        className="navLink navlink-contact"
+        to="/contact"
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+      
+      >
+        Contact
+      </NavLink>
             </Nav>
           </Navbar.Collapse>
         </Container>
