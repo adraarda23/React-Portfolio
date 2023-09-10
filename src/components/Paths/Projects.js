579338d21bec96784491045234d58fe2871f98b2
adraarda23 <ardaaydinkilinc@gmail.com>
2023-09-10 09:50:17 +0300

    Project page converted to dynamic

--- src/components/Paths/Projects.js
+++ src/components/Paths/Projects.js
@@ -3,6 +3,24 @@ import "../Styles/Projects.css";
 import tempImage from "../../images/download.png";
 
 function Projects() {
+  //Resim yoksa default resim koyar
+  const projectData = [
+    {
+      title: "Hafize Ana",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Error non id pariatur quis cumque praesentium aperiam fugit quisquam adipisci eum nihil, maxime illo eligendi esse quaerat! Dolorem aspernatur facere enim.",
+      link: "https://github.com/adraarda23/btu-sks-telegram-bot/tree/main",
+      image: "URL_BURAYA", 
+    },
+    {
+      title: "Başka Bir Proje",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Error non id pariatur quis cumque praesentium aperiam fugit quisquam adipisci eum nihil, maxime illo eligendi esse quaerat! Dolorem aspernatur facere enim.",
+      link: "https://github.com/example/project",
+      
+    },
+  ];
+
   return (
     <div className="projects">
       <div className="projects-title">
@@ -13,63 +31,24 @@ function Projects() {
         </p>
       </div>
       <div className="projects-body">
-        <div className="project__inner-body">
-          <div className="project__inner-body-left">
-            <img className="project-image" src={tempImage} alt="projectImage" />
+        {projectData.map((project, index) => (
+          <div className="project__inner-body" key={index}>
+            <div className="project__inner-body-left">
+              <img
+                className="project-image"
+                src={project.image || tempImage}
+                alt="projectImage"
+              />
+            </div>
+            <div className="project__inner-body-right">
+              <h3>{project.title}</h3>
+              <p>{project.description}</p>
+              <button className="case-study-btn">
+                <a href={project.link}>Case Study</a>
+              </button>
+            </div>
           </div>
-          <div className="project__inner-body-right">
-            <h3>Hafize Ana</h3>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Error non
-              id pariatur quis cumque praesentium aperiam fugit quisquam
-              adipisci eum nihil, maxime illo eligendi esse quaerat! Dolorem
-              aspernatur facere enim.
-            </p>
-            <button className="case-study-btn">
-              <a href="https://github.com/adraarda23/btu-sks-telegram-bot/tree/main">
-                Case Study
-              </a>
-            </button>
-          </div>
-        </div>
-        <div className="project__inner-body">
-          <div className="project__inner-body-left">
-            <img className="project-image" src={tempImage} alt="projectImage" />
-          </div>
-          <div className="project__inner-body-right">
-            <h3>Hafize Ana</h3>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Error non
-              id pariatur quis cumque praesentium aperiam fugit quisquam
-              adipisci eum nihil, maxime illo eligendi esse quaerat! Dolorem
-              aspernatur facere enim.
-            </p>
-            <button className="case-study-btn">
-              <a href="https://github.com/adraarda23/btu-sks-telegram-bot/tree/main">
-                Case Study
-              </a>
-            </button>
-          </div>
-        </div>
-        <div className="project__inner-body">
-          <div className="project__inner-body-left">
-            <img className="project-image" src={tempImage} alt="projectImage" />
-          </div>
-          <div className="project__inner-body-right">
-            <h3>Hafize Ana</h3>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Error non
-              id pariatur quis cumque praesentium aperiam fugit quisquam
-              adipisci eum nihil, maxime illo eligendi esse quaerat! Dolorem
-              aspernatur facere enim.
-            </p>
-            <button className="case-study-btn">
-              <a href="https://github.com/adraarda23/btu-sks-telegram-bot/tree/main">
-                Case Study
-              </a>
-            </button>
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   );
