fa61dca3e5c8c2efb982d7639f63ff36ae059f0b
adraarda23 <ardaaydinkilinc@gmail.com>
2023-09-10 13:25:20 +0300

    Dynamic upgrades applied

--- src/components/Paths/CaseStudy.js
+++ src/components/Paths/CaseStudy.js
@@ -2,6 +2,7 @@ import React from "react";
 import { useParams } from "react-router-dom";
 import "../Styles/CaseStudy.css";
 import tempImage from "../../images/download.png";
+import { Link } from "react-router-dom";
 
 function CaseStudy() {
   const { projectId } = useParams();
@@ -14,6 +15,8 @@ function CaseStudy() {
       link: "https://github.com/adraarda23/btu-sks-telegram-bot/tree/main",
       tools: ["Python", "SQL Basics"],
       image: "https://picsum.photos/400/300", // Resim yolunu ekledik
+      activeLink:
+        "https://github.com/adraarda23/btu-sks-telegram-bot/tree/main", //Buraya aktif kullanımdaki hali gelecek
     },
     "other-project": {
       title: "Başka Bir Proje",
@@ -30,6 +33,7 @@ function CaseStudy() {
   if (!project) {
     return <div>Proje bulunamadı.</div>;
   }
+  console.log(project.activeLink?console.log(1):console.log(2))
 
   return (
     <div className="main-case-study">
@@ -40,35 +44,41 @@ function CaseStudy() {
           includes the Project Overview, Tools Used and Live Links to the
           official product.
         </p>
-            <button className="case-study-btn mt-4 responsive-btn">
-                
+        <button className="case-study-btn mt-4 responsive-btn">
           <a href={project.link}>GitHub Link</a>
-            </button>
+        </button>
       </div>
       <div className="case-study-body">
-          <img src={project.image || tempImage} alt={project.title} /> {/* Resmi dinamik olarak alıyoruz */}
-          <div className="case-study-description">
-            <h3>Project Overview</h3>
-            <p>{project.description}</p>
-          </div>
-          <div className="case-study-tools-used">
-            <h3>Tools Used</h3>
-            <ul className="list-unstyled tools-used">
-              {project.tools.map((tool, index) => (
-                <li className="tools-box" key={index}>{tool}</li>
-              ))}
-            </ul>
-          </div>
-          <div className="case-study-see-live">
-            <h3>See Live</h3>
-            <div className="case-study-see-live-links">
-              <button className="case-study-btn m-3 responsive-btn"><a href="">Live Link</a></button>
-              <button className="case-study-btn m-3 responsive-btn"><a href="">Go Back</a></button>
-              
-              
-            </div>
-          </div>
+        <img src={project.image || tempImage} alt={project.title} />{" "}
+        {/* Resmi dinamik olarak alıyoruz */}
+        <div className="case-study-description">
+          <h3>Project Overview</h3>
+          <p>{project.description}</p>
         </div>
+        <div className="case-study-tools-used">
+          <h3>Tools Used</h3>
+          <ul className="list-unstyled tools-used">
+            {project.tools.map((tool, index) => (
+              <li className="tools-box" key={index}>
+                {tool}
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="case-study-see-live">
+  {project.activeLink?(<h3>See Live</h3>):(<h3>Live Link Unavailable</h3>)}
+  <div className="case-study-see-live-links">
+    {project.activeLink ? (
+      <button className="case-study-btn m-3 responsive-btn">
+        <a href={project.activeLink}>Live Link</a>
+      </button>
+    ) : null}
+    <button className="case-study-btn m-3 responsive-btn">
+      <Link to="/projects">Go Back</Link>
+    </button>
+  </div>
+</div>
+      </div>
     </div>
   );
 }
