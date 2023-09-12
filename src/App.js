70e1ee631f21a9cc271dc6ae60f959ad12ba4f65
adraarda23 <ardaaydinkilinc@gmail.com>
2023-09-12 13:51:45 +0300

    Animation effect decided

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React from "react";
-import { Routes, Route,Router} from "react-router-dom";
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
 import AnimatedRoutes from "./AnimatedRoutes";
-
+import "./App.css"
 function App() {
   return (
     <div className="App">
--- src/components/Animation/PageTransitions.js
+++ src/components/Animation/PageTransitions.js
@@ -1,13 +1,13 @@
-import React from 'react';
 import { motion } from 'framer-motion';
 
 const PageTransition = ({ children }) => {
   return (
     <motion.div
-      initial={{ opacity: 0., x: '100%' }}
-      animate={{ opacity: 1, x: '0%' }}
-      exit={{ opacity: 0, x: '-100%' }}
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
       transition={{ duration: 0.3 }}
+      style={{ backgroundColor: 'rgb(220, 220, 220)' }}
     >
       {children}
     </motion.div>
