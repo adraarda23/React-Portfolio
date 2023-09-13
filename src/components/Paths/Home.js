7ccb4cf0fe87880e4d77814bd4cc2fe9cae5913e
adraarda23 <ardaaydinkilinc@gmail.com>
2023-09-14 00:20:09 +0300

    extra animation added and text animation updated

--- src/components/Paths/Home.js
+++ src/components/Paths/Home.js
@@ -1,16 +1,33 @@
 import React from "react";
+import { motion } from "framer-motion";
 import "../Styles/Home.css";
 import PageTransition from "../Animation/PageTransitions";
 
 function Home() {
+  const variants = {
+    hidden: { opacity: 0, y: -100 }, 
+    visible: { opacity: 1, y: 0, transition: { duration: 1, delay: 3 } },
+  };
+
   return (
     <PageTransition>
       <div className="home-container">
         <div className="home">
-          <h1 className="intro-text" data-text="I'M ARDA KILINÇ">
+          <motion.h1
+            className="intro-text"
+            data-text="I'M ARDA KILINÇ"
+            initial="hidden"
+            animate="visible"
+            variants={variants}
+          >
             <mark className="mark">I'M ARDA KILINÇ</mark>
-            </h1>
-          <p>I like making fun, and interactive things with code.</p>
+          </motion.h1>
+          <motion.p
+            initial={{ opacity: 0, x: -100 }}
+            animate={{ opacity: 1, x: 0, transition: { duration: 1, delay: 4 } }}
+          >
+            I like making fun, and interactive things with code.
+          </motion.p>
         </div>
       </div>
     </PageTransition>
