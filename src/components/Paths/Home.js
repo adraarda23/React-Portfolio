e47eba83555c20256449389a61f255c54b9ce305
adraarda23 <ardaaydinkilinc@gmail.com>
2023-09-13 19:33:37 +0300

    Home Page changed and text animation added

--- src/components/Paths/Home.js
+++ src/components/Paths/Home.js
@@ -2,19 +2,18 @@ import React from "react";
 import "../Styles/Home.css";
 import PageTransition from "../Animation/PageTransitions";
 
-
 function Home() {
   return (
     <PageTransition>
       <div className="home-container">
         <div className="home">
-          <h1>Hi, I'm Arda         
-          </h1>
+          <h1 className="intro-text" data-text="I'M ARDA KILINÇ">
+            <mark className="mark">I'M ARDA KILINÇ</mark>
+            </h1>
           <p>I like making fun, and interactive things with code.</p>
-          
         </div>
       </div>
-      </PageTransition>
+    </PageTransition>
   );
 }
 
