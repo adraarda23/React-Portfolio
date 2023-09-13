5082c8041c635b0a9e514bd161e4d9fa5bb98092
adraarda23 <ardaaydinkilinc@gmail.com>
2023-09-13 12:54:27 +0300

    Navbar animations added when page first time load

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,10 @@
-import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
-import { NavLink, useLocation } from "react-router-dom";
+import React, { useEffect, useState } from "react";
 import "./Styles/NavBar.css";
 import MainLogo from "../images/main.png";
-import { useState, useEffect } from "react";
+import { motion } from "framer-motion";
+import { NavLink, useLocation } from "react-router-dom";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import { useAnimation } from "framer-motion";
 
 function useActiveLink() {
   const location = useLocation();
@@ -22,9 +22,10 @@ function useActiveLink() {
   return activeLink;
 }
 
+
 function NavBar() {
   const activeLink = useActiveLink();
-  console.log(activeLink);
+  const [homeLinkActive, setHomeLinkActive] = useState(true);
 
   const handleMouseEnter = () => {
     if (activeLink) {
@@ -38,27 +39,68 @@ function NavBar() {
     }
   };
 
+  const logoVariants = {
+    hidden: { opacity: 0, x: -50 },
+    visible: { opacity: 1, x: 0 },
+  };
+
+  const textVariants = {
+    hidden: { opacity: 0, x: -50 },
+    visible: { opacity: 1, x: 0, transition: { delay: 0.5 } },
+  };
+
+  const controls = useAnimation();
+  const textControls = useAnimation();
+
+  useEffect(() => {
+    setTimeout(() => {
+      controls.start("visible");
+      textControls.start("visible");
+    }, 1000);
+  }, []);
+
+  useEffect(() => {
+    // "Home" linkine özel sınıfı ekler
+    setTimeout(() => {
+      setHomeLinkActive(false); // 3 saniye sonra sınıfı kaldırır
+    }, 2250); // 3000 milisaniye (3 saniye) gecikme
+  }, []);
+
   return (
     <nav className="mainNavbar">
       <Navbar expand="lg" className="bg-dark text-light">
         <Container>
           <Navbar.Brand href="/">
-            <img src={MainLogo} className="mainLogo" alt="Logo" />
+            <motion.div
+              className="mainLogo"
+              variants={logoVariants}
+              initial="hidden"
+              animate={controls}
+            >
+              <img src={MainLogo} alt="Logo" />
+            </motion.div>
           </Navbar.Brand>
           <Navbar.Toggle
             aria-controls="basic-navbar-nav"
             className="custom-toggler-icon"
-            style={{ filter: 'brightness(0) invert(1)' }}
+            style={{ filter: "brightness(0) invert(1)" }}
           />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto NavBar">
               <NavLink
-                className="navLink navlink-home"
+                className={`navLink navlink-home ${homeLinkActive ? 'home-timer-active' : ''}`}
                 to="/"
                 onMouseEnter={handleMouseEnter}
                 onMouseLeave={handleMouseLeave}
               >
-                Home
+                <motion.span
+                  className="navText"
+                  variants={textVariants}
+                  initial="hidden"
+                  animate={textControls}
+                >
+                  Home
+                </motion.span>
               </NavLink>
               <NavLink
                 className="navLink navlink-about"
@@ -66,7 +108,14 @@ function NavBar() {
                 onMouseEnter={handleMouseEnter}
                 onMouseLeave={handleMouseLeave}
               >
-                About
+                <motion.span
+                  className="navText"
+                  variants={textVariants}
+                  initial="hidden"
+                  animate={textControls}
+                >
+                  About
+                </motion.span>
               </NavLink>
               <NavLink
                 className="navLink navlink-project"
@@ -74,7 +123,14 @@ function NavBar() {
                 onMouseEnter={handleMouseEnter}
                 onMouseLeave={handleMouseLeave}
               >
-                Projects
+                <motion.span
+                  className="navText"
+                  variants={textVariants}
+                  initial="hidden"
+                  animate={textControls}
+                >
+                  Projects
+                </motion.span>
               </NavLink>
               <NavLink
                 className="navLink navlink-contact"
@@ -82,7 +138,14 @@ function NavBar() {
                 onMouseEnter={handleMouseEnter}
                 onMouseLeave={handleMouseLeave}
               >
-                Contact
+                <motion.span
+                  className="navText"
+                  variants={textVariants}
+                  initial="hidden"
+                  animate={textControls}
+                >
+                  Contact
+                </motion.span>
               </NavLink>
             </Nav>
           </Navbar.Collapse>
